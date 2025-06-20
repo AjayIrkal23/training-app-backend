f7c77c58d4aa6b06a01c96ebeadb8b260da69679
Ajay Irkal <103778100+AjayIrkal23@users.noreply.github.com>
2025-06-20 16:06:12 +0530

    update

--- src/workers/assignTrainingWorker.ts
+++ src/workers/assignTrainingWorker.ts
@@ -1,6 +1,6 @@
 // workers/assignTrainingWorker.ts
 import { Worker } from "bullmq";
-import { redis } from "utils/redis";
+import { redis } from "../utils/redis";
 import { assignTrainingsToUsers } from "./../jobs/scheduleAssignTraining";
 
 export const assignTrainingWorker = new Worker(
